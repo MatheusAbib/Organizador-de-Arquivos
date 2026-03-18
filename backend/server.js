077d052f06ee581e2ec3c63d787baf7a1d51211a
MATHEUS BILITARDO ABIB <97857@pmogi.local>
2026-03-18 11:51:36 -0300

    Ajustes na conexão com o banco de dados no clever cloud

--- backend/server.js
+++ backend/server.js
@@ -132,10 +132,11 @@ const upload = multer({
 });
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    port: process.env.MYSQL_ADDON_PORT || 3306
 });
 
 db.connect((err) => {
