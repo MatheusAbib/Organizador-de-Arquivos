4e49d35cfffe6c78f2e2d667cb2ed59c8c93e339
MATHEUS BILITARDO ABIB <97857@pmogi.local>
2026-03-19 09:51:35 -0300

    Ajustando erro de CORS

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,13 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'usuario-id']
 }));
 
-app.options('*', cors());
+app.options('*', (req, res) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, usuario-id');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(200);
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
