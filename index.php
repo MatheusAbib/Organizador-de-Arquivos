806aeb36148c65594ea09fb5f31b0f044e7e6f55
MATHEUS BILITARDO ABIB <97857@pmogi.local>
2026-03-19 10:12:21 -0300

    Ajustando erro de CORS

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
-include_once __DIR__ . '/frontend/index.html';
+$request = $_SERVER['REQUEST_URI'];
+
+$request = strtok($request, '?');
+
+if ($request == '/') {
+    include __DIR__ . '/frontend/index.html';
+    exit;
+}
+
+$file = __DIR__ . '/frontend' . $request;
+
+if (file_exists($file) && is_file($file)) {
+    header('Content-Type: text/html');
+    readfile($file);
+    exit;
+}
+
+http_response_code(404);
+echo "Página não encontrada";
 exit;
 ?>
